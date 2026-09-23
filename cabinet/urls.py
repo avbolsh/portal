cf6bc108d6b74dbf2d40c950c0bfe797ba97cad8
Alexey Bolshakov <avbolshakov@gmail.com>
2026-09-23 09:15:03 +0300

    добавил требование при входе ввести временный пароль

--- cabinet/urls.py
+++ cabinet/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import login_view, dashboard_view
+from .views import login_view, dashboard_view, totp_view
 
 urlpatterns = [
         path("login/", login_view, name="cabinet-login"),
         path("", dashboard_view, name="cabinet-dashboard"),
+        path("totp/", totp_view, name="cabinet-totp"),
         ]
--- cabinet/views.py
+++ cabinet/views.py
@@ -1,6 +1,8 @@
+import pyotp
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
 
 
 def login_view(request):
@@ -11,6 +13,9 @@ def login_view(request):
                             username=username, 
                             password=password)
         if user is not None:
+            if user.totp_secret:
+                request.session["auth_user_id"] = user.id
+                return redirect("cabinet-totp")
             login(request, user)
             return redirect("cabinet-dashboard")
     return render(request, "cabinet/login.html")
@@ -19,3 +24,18 @@ def login_view(request):
 @login_required
 def dashboard_view(request):
     return render(request, "cabinet/dashboard.html")
+
+def totp_view(request):
+    if request.method == "POST":
+        code = request.POST.get("code")
+        user_id = request.session.get("auth_user_id")
+        if not user_id:
+            return redirect("cabinet-login")
+        User = get_user_model()
+        user = User.objects.get(id=user_id)
+        totp = pyotp.TOTP(user.totp_secret)
+        if totp.verify(code):
+            login(request, user)
+            del request.session["auth_user_id"]
+            return redirect("cabinet-dashboard")
+    return render(request, "cabinet/totp.html")

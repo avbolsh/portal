e164efbffb46bd07a71cbd017c500265ca7ab0ba
Alexey Bolshakov <avbolshakov@gmail.com>
2026-09-23 10:52:37 +0300

    добавил доступ к api по токенам

--- api/views.py
+++ api/views.py
@@ -5,6 +5,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from .serializers import EmployeeCreateInputSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 User = get_user_model()
 
@@ -14,6 +16,8 @@ class PingView(APIView):
         return Response({"status": "ok"})
 
 class EmployeeCreateView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def post(self, request):
         serializer = EmployeeCreateInputSerializer(data=request.data)
--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
+    "rest_framework.authtoken",
     "api.apps.ApiConfig",
     'cabinet.apps.CabinetConfig',
 ]
@@ -130,3 +131,12 @@ MAILERS = {
 }
 
 AUTH_USER_MODEL = "cabinet.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+}
